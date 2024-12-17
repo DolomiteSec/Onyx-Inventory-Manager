ab6b0f2e130116e2f12c2709537159612dae4190
DolomiteSec <127999706+DolomiteSec@users.noreply.github.com>
2024-12-17 02:23:32 -0600

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -32,6 +32,4 @@ app.use("/api/invoices", invoiceRoutes);
 
 // Start the Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-    console.log(`🚀 Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
